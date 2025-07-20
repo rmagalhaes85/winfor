931d26e3a8d36e1f39f3358189d8434c2ed82a29
rmagalhaes85 <rmagalhaes85@gmail.com>
2025-07-20 12:09:54 -0300

    Implementar autorização no serviço winfor-api

--- winfor-api/src/main/java/com/winfor/GreetingResource.java
+++ winfor-api/src/main/java/com/winfor/GreetingResource.java
@@ -1,16 +1,33 @@
 package com.winfor;
 
+import io.quarkus.security.identity.SecurityIdentity;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/api")
 public class GreetingResource {
 
+    @Inject
+    SecurityIdentity securityIdentity;
+
     @GET
+    @Path("/public/hello")
+    @PermitAll
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return "Hello from Quarkus REST!!";
+    }
+
+    @GET
+    @Path("/private/hellovip")
+    @RolesAllowed("admin_winfor")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloVip() {
+        return "Hello VIP";
     }
 }
