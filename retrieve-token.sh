6f3c084b9cd63f13f8db9b2eb3f4a88d08de84c8
rmagalhaes85 <rmagalhaes85@gmail.com>
2025-07-20 12:10:50 -0300

    Incluir script auxiliar para testes
    Incluir script auxiliar para testes de autenticação e autorização via
    linha de comando

---
+++ retrieve-token.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -v -X POST "http://localhost:8090/realms/winfor/protocol/openid-connect/token" \
+     -H "Content-Type: application/x-www-form-urlencoded" \
+     -d "client_id=winfor-app" \
+     -d "client_secret=GhnBwLbKKgTWoOO8HvPFmwLRdACBf15W" \
+     -d "grant_type=password" \
+     -d "username=admin1" \
+     -d "password=password" | jq
+
