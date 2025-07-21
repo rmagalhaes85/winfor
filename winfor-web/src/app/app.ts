3d2b7d87e2c5f9cb92319024e0473d487224f20e
rmagalhaes85 <rmagalhaes85@gmail.com>
2025-07-21 17:58:04 -0300

    Incluir exemplos + experimento com tailwindcss

--- winfor-web/src/app/app.ts
+++ winfor-web/src/app/app.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 //import { AuthService } from './auth.service';
 import { KeycloakService } from 'keycloak-angular';
@@ -12,9 +12,32 @@ import { KeycloakService } from 'keycloak-angular';
 })
 export class App {
   protected readonly title = signal('winfor-web');
+  sidebarOpen = false;
+  sidebarVisible = true;
 
   //constructor(public auth: AuthService) {}
-  constructor(public keycloakService: KeycloakService) { }
+  constructor(public keycloakService: KeycloakService) {
+    this.checkScreenSize();
+  }
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  @HostListener('window:resize', [])
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    if (window.innerWidth >= 1024) {
+      this.sidebarVisible = true;
+      this.sidebarOpen = true; // Always open on large
+    } else {
+      this.sidebarVisible = true;
+      this.sidebarOpen = false; // Hidden by default on small
+    }
+  }
 
   async login() {
     //this.auth.login();
