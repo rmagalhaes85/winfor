0a0e2a995b0cf70fe149fd069b089940317eae76
rmagalhaes85 <rmagalhaes85@gmail.com>
2025-07-23 18:03:38 -0300

    Criar diretório com scripts de auxílio a testes

---
+++ scripts/call-usuarios.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+set -x
+
+access_token=$(./retrieve-token.sh | jq -r ".access_token")
+[ $? -ne 0 ] && { exit "Erro na leitura do token"; exit 1; }
+curl -v http://localhost:8080/usuario/ -H "Authorization: Bearer ${access_token}"
