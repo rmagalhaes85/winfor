c7739f4b374fa81a9c6a11a55134637f59c9afad
rmagalhaes85 <rmagalhaes85@gmail.com>
2025-07-24 13:53:21 -0300

    Alterar chamada de testes à API de usuários

--- scripts/call-usuarios.sh
+++ scripts/call-usuarios.sh
@@ -5,4 +5,4 @@ set -x
 
 access_token=$(./retrieve-token.sh | jq -r ".access_token")
 [ $? -ne 0 ] && { exit "Erro na leitura do token"; exit 1; }
-curl -v http://localhost:8080/usuario/ -H "Authorization: Bearer ${access_token}"
+curl -v http://localhost:8080/api/usuario/ -H "Authorization: Bearer ${access_token}"
