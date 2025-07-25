54e50cfb0989c30875515001b8753c3c6d16b398
rmagalhaes85 <rmagalhaes85@gmail.com>
2025-07-25 19:10:27 -0300

    Incluir tratamento de erros

--- winfor-web/src/app/usuario/usuario.service.ts
+++ winfor-web/src/app/usuario/usuario.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
+import { throwError, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Usuario } from './usuario';
 
 @Injectable({
@@ -17,7 +18,8 @@ export class UsuarioService {
   }
 
   addUsuario(username: string): Observable<Usuario> {
-    return this.httpClient.post<Usuario>(`${this.apiUrl}/criar`, username);
+    return this.httpClient.post<Usuario>(`${this.apiUrl}/`, username)
+      .pipe(catchError(this.handleCreationError));
   }
 
   updateUsuario(usuario: Usuario): Observable<Usuario> {
@@ -26,5 +28,25 @@ export class UsuarioService {
 
   deleteUsuario(id: string): Observable<void> {
     return this.httpClient.delete<void>(`${this.apiUrl}/${id}`);
+  }
+
+  private handleCreationError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      return throwError(() => new Error("Falha de conexão. Por favor, tente novamente"));
+    } else if (error.status === HttpStatusCode.Conflict) {
+      return throwError(() => new Error("Já existe um usuário com o login informado"));
+    } else {
+      console.error(`Erro na criação do usuário: ${error.error}`);
+      return throwError(() => new Error("Ocorreu um erro na criação do usuário"));
+    }
+  }
+
+  private handleRetrievalError(error: HttpErrorResponse) {
+    if (error.status === HttpStatusCode.NotFound) {
+      return throwError(() => new Error("Usuário não encontrado"));
+    } else {
+      console.error(`Erro na leitura do usuário: ${error.error}`);
+      return throwError(() => new Error("Ocorreu um erro. Por favor, tente novamente"));
+    }
   }
 }
