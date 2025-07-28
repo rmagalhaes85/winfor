d6554eaaec2a48fdbe7d3311b3edb0aa42836257
rmagalhaes85 <rmagalhaes85@gmail.com>
2025-07-28 12:58:40 -0300

    Incluir endpoint de listagem de cursos

--- winfor-api/src/main/java/com/winfor/curso/CursoResource.java
+++ winfor-api/src/main/java/com/winfor/curso/CursoResource.java
@@ -1,5 +1,7 @@
 package com.winfor.curso;
 
+import java.util.List;
+
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -9,10 +11,18 @@ import jakarta.ws.rs.Produces;
 public class CursoResource {
 
     @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Curso> listAll() {
+        return Curso.listAll();
+    }
+
+    @GET
     @Path("/{codCurso}")
     @Produces(MediaType.APPLICATION_JSON)
     public Curso getByCodCurso(String codCurso) {
         return Curso.findByCodCurso(codCurso);
     }
+
 }
 
